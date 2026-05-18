f0aa22584331ac2c2eaa541f0068991e0ef854d9
evalAnjal <kinganjalap@gmail.com>
2026-05-18 08:23:45 +0545

    Error fixes and Host ready (CHECKPOINT MVP)

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const config: NextConfig = {
   turbopack: {},
-  typescript: {
-    // !! WARNING !!
-    // Dangerously allow production builds to successfully complete 
-    // even if your project has Type errors.
-    ignoreBuildErrors: true,
-  },
 };
 
 export default config;
